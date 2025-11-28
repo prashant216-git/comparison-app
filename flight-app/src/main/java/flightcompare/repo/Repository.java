1e368f3ac2910fc37ac2a94afc25d9819984483e
prashant216-git <saxenaprashant216@gmail.com>
2025-11-28 08:32:24 +0530

    Delete flight-app directory

--- flight-app/src/main/java/flightcompare/repo/Repository.java
+++
@@ -1,9 +0,0 @@
-package flightcompare.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import flightcompare.model.Flight;
-
-public interface Repository extends JpaRepository<Flight, Long> {
-
-}
